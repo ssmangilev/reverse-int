57011b282847e73982a34e8ac73ed511e051d574
Sergey Mangilev <ssmangilev@gmail.com>
2021-03-06 18:52:55 +0100

    init: task 'reversed-int' solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let n_array = String(n).split("")
+  let result = []
+  for (let i=n_array.length-1;i>=0;i--) {
+    result += n_array[i];
+  }
+  return Number(result);
 }
